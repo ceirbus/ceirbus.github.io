748925f7684fa68ac3eeec72b92242c97b2e10f9
Will Faircloth <ceirbus@gmail.com>
2020-08-13 21:08:55 -0400

    add routing to fix github pages deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'table', pathMatch: 'full'},
+  { path: 'table', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
